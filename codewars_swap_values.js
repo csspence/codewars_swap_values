67814db82c0ec695ac8195647c9f54361dbcfc2b
Crew Spence <csspence123@gmail.com>
2020-10-21 04:19:48 -0500

    Debugged toy problem.

--- codewars_swap_values.js
+++ codewars_swap_values.js
@@ -3,10 +3,8 @@ I would like to be able to pass an array with two elements to my swapValues func
 
 Can you figure out what's wrong here?
 */
-
-function swapValues() {
-    var args = Array.prototype.slice.call(arguments);
-    var temp = args[0];
-    args[0] = args[1];
-    args[1] = temp;
+const swapValues = (array) => {
+    var temp = array[0];
+    array[0] = array[1];
+    array[1] = temp;
 }
